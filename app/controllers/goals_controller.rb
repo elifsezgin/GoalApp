a210b7e6f3d678b4e8f67e39542381e84da260de
App Academy Student <student@appacademy.io>
2016-12-12 16:00:43 -0800

    Add goals controller and tests

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,59 @@
 class GoalsController < ApplicationController
 
 
+  def index
+    @goals = Goal.where(user_id: current_user.id)
+    render :index
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+    render :show
+  end
+
+  def new
+    @goal = Goal.new
+    render :new
+  end
+
+  def create
+    @goal = Goal.new(goal_params)
+    @goal.user_id = current_user.id
+    if @goal.save
+      redirect_to goal_url(goal)
+    else
+      flash[:errors] = @goal.errors.full_messages
+      redirect_to new_goal_url
+    end
+  end
+
+  def edit
+    @goal = Goal.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.user_id == current_user.id
+      if @goal.update(goal_params)
+        redirect_to goal_url(@goal)
+      else
+        flash.now[:errors] = @goal.errors.full_messages
+        render :edit
+      end
+    end
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.delete
+    redirect_to goals_url
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:title, :description, :is_private, :is_completed)
+  end
 
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
+  let(:kathy) {User.create!(username: 'kalu', password:"123456")}
+  describe "GET #new" do
+    it "renders new goals page" do
+      get :new, goal: {}
+
+      expect(response).to render_template("new")
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe "POST #create" do
+    context 'when logged in' do
+      before(:each) do
+        allow(controller).to receive(:current_user) { kathy }
+      end
+
+      context "with valid params" do
+        it "renders goal page" do
+          post :create, goal: {title: "test goal", description: 'test test'}
+          goal = Goal.find_by_title('test goal')
+
+          expect(response).to redirect_to(user_url(user))
+          expect(response).to have_http_status(302)
+        end
+      end
+      context "with invalid params" do
+        it "renders new goal page" do
+          post :create, goal: {title: "", description: 'test test'}
+
+          expect(response).to render_template("new")
+          expect(flash[:errors]).to be_present
+        end
+      end
+    end
+    context 'when not logged in' do
+      it 'redirects to log in page' do
+        get :new, goal: {}
+
+        expect(response).to redirect_to(new_session_url)
+      end
+    end
+  end
 
 end
